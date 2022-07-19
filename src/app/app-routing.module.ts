fb210a8e27b64a0716b1e340f54d6ce87a1301eb
Dylan Martin <107432740+Dmartin348@users.noreply.github.com>
2022-07-19 15:52:59 -0400

    handoff to Glenn

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ListsComponent } from './components/lists/lists.component';
+import { LoginComponent } from './components/login/login.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RegisterComponent } from './components/register/register.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomeComponent
+},
+{  path: 'login',
+  component: LoginComponent
+},
+{  path: 'profile',
+   component:  ProfileComponent
+},
+{  path: 'lists',
+   component:  ListsComponent
+},
+{  path: 'register',
+   component:  RegisterComponent
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
