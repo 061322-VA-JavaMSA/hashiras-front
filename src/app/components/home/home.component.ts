2e73d6cb2522fd0610caba4d4c157d4116261328
Dylan Martin <107432740+Dmartin348@users.noreply.github.com>
2022-07-21 12:44:03 -0400

    baseline styling

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 //need this start
 import { Component, OnInit } from '@angular/core';
+import { POINT_CONVERSION_UNCOMPRESSED } from 'constants';
 
 @Component({
   selector: 'app-home',
@@ -15,47 +16,18 @@ export class HomeComponent implements OnInit {
 }
 //need this end
 
-//write a function that populates the currentSeason table with the current season's data using the jikan v4 api
+async function getCurrentSeason(data){
+  let response = await fetch('https://api.jikan.moe/v4/seasons/now', {
+    method: 'GET',
+    credentials: 'same-origin'
+  });
+  if(response.ok){
+    let data = await response.json();
 
-// async function getCurrentSeason(data) {
-//   let response = await fetch('https://api.jikan.moe/v4/season/current', {
-//   // method: 'GET',  
-//   credentials: 'include'
-//   });
+    console.log(response);
 
-//   if(response.status == 200) {
-//     let json = await response.json();
-//     // console.log(json);
-//     // data.currentSeason = json;
-//     populateTable(data);
-//   } else {
-//     console.log('Unable to fetch season data');
-//   }
-// }
-
-// function populateTable(data) {
-//   let table = document.getElementById('currentSeason');
-
-//   data.array.forEach(element => {
-//     let tr = document.createElement('tr');
-//     let tdAnimeId = document.createElement('td');
-//     let tdTitle = document.createElement('td');
-//     let tdGenre1 = document.createElement('td');
-//     let tdGenre2 = document.createElement('td');
-//     let tdGenre3 = document.createElement('td');
-
-//     tdAnimeId.innerHTML = element.anime_id;
-//     tdTitle.innerHTML = element.title;
-//     tdGenre1.innerHTML = element.genre[0];
-//     tdGenre2.innerHTML = element.genre[1];
-//     tdGenre3.innerHTML = element.genre[2];
-
-//     tr.appendChild(tdAnimeId);
-//     tr.appendChild(tdTitle);
-//     tr.appendChild(tdGenre1);
-//     tr.appendChild(tdGenre2);
-//     tr.appendChild(tdGenre3);
-
-//     table.appendChild(tr);
-//   });
-// }
+    // populateTable(data);
+  } else {
+    console.log('error');
+  }
+}
