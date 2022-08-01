1c740eed5251af0246f0fec9138ccad830ba1f00
lanem123 <lanedmcspadden@gmail.com>
2022-08-01 07:35:17 -0500

    Final merge

--- src/app/models/comment.ts
+++ src/app/models/comment.ts
@@ -1,9 +1,9 @@
 export class Comment {
     public anime_id : number;
-    public author : string;
+    public author : number;
     public comment : string;
 
-    constructor(anime_id: number, author: string, comment: string) {
+    constructor(anime_id: number, author: number, comment: string) {
         this.anime_id = anime_id;
         this.author = author;
         this.comment = comment;
